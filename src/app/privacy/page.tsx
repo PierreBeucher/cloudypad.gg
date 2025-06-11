2c2df4e97f62c1085b524fd9d3e89f7e9fa80d70
Pierre beucher <pierre@crafteo.io>
2025-06-11 17:41:49 +0200

    fix build

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -6,12 +6,9 @@ export default function PrivacyPage() {
       <div className="max-w-3xl mx-auto px-4">
         <h1 className="text-4xl font-bold mb-8 text-center">Cloudy Pad - Privacy Policy</h1>
         <section className="mb-8">
-          <p className="mb-4">Cloudy Pad operates the Cloudy Pad app website, which provides Cloud Gaming services (later referred as "our Service" or "the Service").</p>
-
+          <p className="mb-4">Cloudy Pad operates the Cloudy Pad app website, which provides Cloud Gaming services (later referred as &quot;our Service&quot; or &quot;the Service&quot;).</p>
           <p className="mb-4">This page is used to inform website visitors regarding our policies with the collection, use, and disclosure of Personal Information if anyone decided to use our Service, the Cloudy Pad app website.</p>
-          
           <p className="mb-4">If you choose to use our Service, then you agree to the collection and use of information in relation with this policy. The Personal Information that we collect are used for providing and improving the Service. We will not use or share your information with anyone except as described in this Privacy Policy.</p>
-          
           <p className="mb-4">The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which is accessible at Website URL, unless otherwise defined in this Privacy Policy.</p>
         </section>
         <section className="mb-8">
@@ -20,11 +17,11 @@ export default function PrivacyPage() {
         </section>
         <section className="mb-8">
           <h2 className="text-2xl font-semibold mb-2">Log Data</h2>
-          <p>We want to inform you that whenever you visit our Service, we collect information that your browser sends to us that is called Log Data. This Log Data may include information such as your computer's Internet Protocol ("IP") address, browser version, pages of our Service that you visit, the time and date of your visit, the time spent on those pages, and other statistics.</p>
+          <p>We want to inform you that whenever you visit our Service, we collect information that your browser sends to us that is called Log Data. This Log Data may include information such as your computer&apos;s Internet Protocol (&quot;IP&quot;) address, browser version, pages of our Service that you visit, the time and date of your visit, the time spent on those pages, and other statistics.</p>
         </section>
         <section className="mb-8">
           <h2 className="text-2xl font-semibold mb-2">Cookies</h2>
-          <p className="mb-2">Cookies are files with small amount of data that is commonly used an anonymous unique identifier. These are sent to your browser from the website that you visit and are stored on your computer's hard drive.</p>
+          <p className="mb-2">Cookies are files with small amount of data that is commonly used an anonymous unique identifier. These are sent to your browser from the website that you visit and are stored on your computer&apos;s hard drive.</p>
           <p>Cookies are currently used to provide the Service. They are not used to target ads or gather personal information. Without cookies, you cannot access the website and use the Service. By using the Service you agree that cookies are used.</p>
         </section>
         <section className="mb-8">
@@ -47,7 +44,7 @@ export default function PrivacyPage() {
           <p>Our Service may contain links to other sites. If you click on a third-party link, you will be directed to that site. Note that these external sites are not operated by us. Therefore, we strongly advise you to review the Privacy Policy of these websites. We have no control over, and assume no responsibility for the content, privacy policies, or practices of any third-party sites or services.</p>
         </section>
         <section className="mb-8">
-          <h2 className="text-2xl font-semibold mb-2">Children's Privacy</h2>
+          <h2 className="text-2xl font-semibold mb-2">Children&apos;s Privacy</h2>
           <p>Our Services do not address anyone under the age of 13. We do not knowingly collect personal identifiable information from children under 13. In the case we discover that a child under 13 has provided us with personal information, we immediately delete this from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact us so that we will be able to do necessary actions.</p>
         </section>
         <section className="mb-8">
