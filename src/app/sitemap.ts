c1bc5cfd6e8330598e0b3e16f466be74c806a9ab
Pierre beucher <pierre@crafteo.io>
2025-10-15 12:10:57 +0200

    remove app.cloudypad.gg/pricing from index

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -10,12 +10,13 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: 'weekly',
       priority: 1,
     },
-    {
-        url: 'https://app.cloudypad.gg/pricing',
-        lastModified: new Date(),
-        changeFrequency: 'weekly',
-        priority: 0.8,
-      },
+    // may prevent bing indexing
+    // {
+    //     url: 'https://app.cloudypad.gg/pricing',
+    //     lastModified: new Date(),
+    //     changeFrequency: 'weekly',
+    //     priority: 0.8,
+    //   },
     {
       url: 'https://cloudypad.gg/privacy',
       lastModified: new Date(),
